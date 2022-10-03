6254c8d2f15845dfad516861cc31eae0e9db8354
Stultz-e <cstultz@oswego.edu>
2022-10-03 01:21:47 -0600

    search fix

--- pages/search.js
+++ pages/search.js
@@ -4,10 +4,10 @@ import Image from 'next/image';
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
 
-import Property from './../components/Property';
-import SearchFilters from './../components/SearchFilters';
-import noresult from '../assets/images/noresult.svg';
+import Property from '../components/Property';
+import SearchFilters from '../components/SearchFilters';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
+import noresult from '../assets/images/noresult.svg';
 
 const Search = ({ properties }) => {
   const [searchFilters, setSearchFilters] = useState(false);
@@ -16,18 +16,18 @@ const Search = ({ properties }) => {
   return (
     <Box>
       <Flex
+        onClick={() => setSearchFilters(!searchFilters)}
         cursor='pointer'
         bg='gray.100'
         borderBottom='1px'
         borderColor='gray.200'
         p='2'
-        fontWeight='bold'
+        fontWeight='black'
         fontSize='lg'
         justifyContent='center'
-        alignItems='center'
-        onClick={() => setSearchFilters((prevFilters) => !prevFilters)}>
+        alignItems='center'>
         <Text>Search Property By Filters</Text>
-        <Icon paddingLeft='2' w='7' as={BsFilter}></Icon>
+        <Icon paddingLeft='2' w='7' as={BsFilter} />
       </Flex>
       {searchFilters && <SearchFilters />}
       <Text fontSize='2xl' p='4' fontWeight='bold'>
@@ -42,12 +42,12 @@ const Search = ({ properties }) => {
         <Flex
           justifyContent='center'
           alignItems='center'
-          flexDirection='column'
+          flexDir='column'
           marginTop='5'
           marginBottom='5'>
-          <Image alt='no result' src={noresult} />
-          <Text fontSize='2xl' marginTop='3'>
-            No Results From
+          <Image src={noresult} />
+          <Text fontSize='xl' marginTop='3'>
+            No Result Found.
           </Text>
         </Flex>
       )}
